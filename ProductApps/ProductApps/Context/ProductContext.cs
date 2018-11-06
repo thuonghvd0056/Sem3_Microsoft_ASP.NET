ea1e10642b96e7b123ffe44f7283c66e3a6a1a72
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2018-11-06 22:52:42 +0700

    asp.net1

---
+++ ProductApps/ProductApps/Context/ProductContext.cs
@@ -0,0 +1,14 @@
+﻿using ProductApps.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductApps.Context
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
